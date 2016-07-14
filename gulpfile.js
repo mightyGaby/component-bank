39ce3ad6d22980d8e6b5bd181000e3c5fd5f54f3
Joe Slater <joe.slater@razorfish.com>
2016-07-14 17:06:10 -0500

    [component-bank] - jade build-out (WIP)

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,39 @@
 // Include gulp
 var gulp = require('gulp');
 
-// Include Our Plugins
+// Gulp Plugins
 var sass = require('gulp-sass'),
 	rename = require("gulp-rename"),
-	cleanCSS = require('gulp-clean-css');
+	cleanCSS = require('gulp-clean-css'),
+    jade = require('gulp-jade');
 
-// Compile Our Sass
+// Compile Sass
 gulp.task('sass', function() {
     return gulp.src(['scss/**/*.scss', 'scss/*.scss', '!scss/variables.scss', '!scss/mixins.scss'])
         .pipe(sass())
         .pipe(gulp.dest('css'));
 });
 
-// Minify Our CSS
+// Minify CSS
 gulp.task('minify-css', function() {
 	return gulp.src(['css/*.css', 'css/modules/*.css'])
   		.pipe(cleanCSS())
   		.pipe(rename({
         	suffix: '.min'
     	}))
-    	.pipe(gulp.dest('css/min/'));
+    	.pipe(gulp.dest('prod/css/min/'));
+});
+
+// Jade templates
+gulp.task('jade', function() {
+    return gulp.src(['templates/*.jade', 'templates/**/*.jade'])
+        .pipe(jade())
+        .pipe(gulp.dest('prod'));
 });
 
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch(['scss/**/*.scss', 'scss/*.scss', '!scss/variables.scss', '!scss/mixins.scss'], ['sass']);
+    gulp.watch(['scss/**/*.scss', 'scss/*.scss', '!scss/variables.scss', '!scss/mixins.scss', 'templates/*.jade', 'templates/**/*.jade'], ['sass', 'jade']);
     gulp.watch('css/*.css', ['minify-css']);
 });
 
