c36c384cb4c0474e6dd3c5fd83f206199aad3aa9
Joe Slater <joe.slater@razorfish.com>
2016-07-20 11:47:11 -0500

    [component-bank] - waves - waves build-out

--- dev/js/tabModule.js
+++ dev/js/tabModule.js
@@ -1,16 +1,19 @@
 (function($){
 	var js = window.js || (window.js = {});
 
-	js.tabModule = function(root, params){
+	js.wavesModule = function(root, params){
 		this.root = $(root);
 
 		this.init();
 	};
 
-	js.tabModule.prototype = {
+	js.wavesModule.prototype = {
 		init: function(){
 			//object variables
-			
+			this.baseurl = 'http://api.worldweatheronline.com/premium/v1/marine.ashx';
+			this.key = 'c0b567aa6a4345abbe7195449161907';
+
+
 		    //setup
 		    this.setup();
 
@@ -19,11 +22,24 @@
 		},
 
 		setup: function() {
-			
+			$.ajax({
+				contentType: "application/json",
+				cache: false,
+				method: "POST",
+				url: this.baseurl + "?key=" + this.key + "&q=28.41,-80.63&format=json&num_of_days=1",
+				//data: JSON.stringify(data),
+				success: $.proxy(this.onsuccess_ajax, this),
+				error: $.proxy(this.onerror_ajax, this)
+			});
 		},
 
 		events:function() {
 			
+		},
+
+		onsuccess_ajax: function(response, textStatus, jqXHR) {
+			console.log(response.data.weather);
+			$(".data").html(response.data);
 		}
 	};
 })(jQuery);
