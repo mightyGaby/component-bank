a8a5999190774a36f9225d6f55e582b7f6c33265
Gaby Ruiz-Funes <gabsmallnmighty@gmail.com>
2016-08-25 12:07:31 -0500

    project titles

--- dev/js/app.js
+++ dev/js/app.js
@@ -3,19 +3,21 @@
 	var js = window.js || (window.js = {});
 
 	js.app = {
-        init: function() {
-            this.initComponents();
+		init: function() {
+			this.displayProjects();
 		},
-        
-        initComponents: function(){
-            // tab module
-            $(".slider").each(function(i, el){
-                new js.slider(el);
-            });
-        }
-    };
 
-    // kickoff app
-    $(document).ready($.proxy(js.app.init, js.app));
+		displayProjects: function(){
+			$('.category_links a').on('click', function(){
+				var category = $(this).attr('class');
+				var selected = '.' + category;
+				$('.projectContainer').children().attr("aria-hidden", "true");
+				console.log($('.projectContainer').find(selected).attr("aria-hidden","false"));
+			});
+		}
+};
+
+	// kickoff app
+	$(document).ready($.proxy(js.app.init, js.app));
 
 })(jQuery);
